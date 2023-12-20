6f21fff61c384f3f8e1f11caacdd17313b83334f
Nintendoboi22 <131544102+Nintendoboi22@users.noreply.github.com>
2023-12-19 23:28:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-document.onkeypress = function (e) { if(e.key == "192") window.location = "https://www.classroom.google.com/h" };
+document.onkeypress = function (e) { if(e.key == "`") window.location = "https://www.classroom.google.com/h" };
