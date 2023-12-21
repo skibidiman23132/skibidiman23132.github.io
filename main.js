1a854f0e12d2486453512159c8227ea8d0f75493
Nintendoboi22 <131544102+Nintendoboi22@users.noreply.github.com>
2023-12-21 17:10:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
+alert("I will not be updating the website for a while. movies yes, but main website updates are a no as im adding the login page :D")
 document.onkeypress = function (e) { if(e.key == "`") window.location = "https://www.classroom.google.com/h" };
-alert:"I will not be updating the website for a while. movies yes, but main website updates are a no as im adding the login page :D"
