811f5a26c5af1b5c1d62752df8489324321ea2f4
Nintendoboi22 <131544102+Nintendoboi22@users.noreply.github.com>
2023-12-21 17:05:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,2 @@
 document.onkeypress = function (e) { if(e.key == "`") window.location = "https://www.classroom.google.com/h" };
+alert:"I will not be updating the website for a while. movies yes, but main website updates are a no as im adding the login page :D"
