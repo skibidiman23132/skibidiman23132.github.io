6b0e1669508d1c789e92fa121f0a1b9c7449cff6
Nintendoboi22 <131544102+Nintendoboi22@users.noreply.github.com>
2024-01-08 14:03:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-alert("press ~ (on the right of the of the 1 key, works on all sites)")
 document.onkeypress = function (e) { if(e.key == "`") window.location = "https://www.classroom.google.com/h" };
+document.onkeypress = function (e) { if(e.key == "~") window.location = "https://www.classroom.google.com/h" };
