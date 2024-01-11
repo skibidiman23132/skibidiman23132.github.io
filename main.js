cb0921a9b5a8d213800b29b8d01c2a159a0bf5f7
nintendoboi22 <petterelijah69@gmail.com>
2024-01-11 16:59:15 -0500

    loging page :)

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 document.onkeypress = function (e) { if(e.key == "`") window.location = "https://www.classroom.google.com/h" };
+let user=document.getElementById("txt_user")
+let password=document.getElementById("txt_pass")
+let button=document.getElementById("imdone")
+let login=document.getElementById("login")
+let maincontent =document. getElementById("content");
+alert(document.cookie);
+    function unlockpage(){
+          maincontent.classList.remove("lock");
+            login.classList.add("lock");
+        }
+        if (document.cookie=="login"){unlockpage();}
+   // document.onkeypress = function (e) { if(e.key == "13") if (login.focus)};
+    button.onclick=()=>{
+        if (user.value=="Admin"&& password.value=="chicken"){
+          unlockpage();
+            document.cookie="login";
+    }
+}
+    
