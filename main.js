15cbe253634760461481cefe8bdde3276caf6bda
nintendoboi22 <petterelijah69@gmail.com>
2024-01-18 16:59:43 -0500

    nothing :)

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let button=document.getElementById("imdone")
 let login=document.getElementById("login")
 let maincontent =document. getElementById("content");
 
-{username:"Student", pasword:"Lobster"}
+// {username:"Student", pasword:"Lobster"}
 
 document.onkeypress = function (e) { if(e.key == "Enter" && login.focus){
   unlockpage();
