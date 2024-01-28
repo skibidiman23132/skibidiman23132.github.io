bf0c1d9f830ed4d1a66f8aabd27894c8a946a51b
Nintendoboi22 <131544102+Nintendoboi22@users.noreply.github.com>
2024-01-27 20:59:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-//object.onkeydown = function (e) { if(e.key == 222) window.location = "https://www.classroom.google.com/h" };
+document.onkeypress = function (e) { if(e.key == "~") window.location = "https://www.classroom.google.com/h" };
 
 let user=document.getElementById("txt_user")
 let password=document.getElementById("txt_pass")
 let button=document.getElementById("imdone")
 let login=document.getElementById("login")
 let maincontent =document. getElementById("content");
-
 let page_cookie={};
 try{
 page_cookie=JSON.parse(document.cookie);
 } catch(error){
       console.error(error);
       page_cookie.login=false;
+      document.cookie=JSON.parse(page_cookie);
       document.cookie=JSON.stringify(page_cookie);
 }
-auth=[{username:"Pretzel", password:"Pookie"},
+auth=[{username:"Student", password:"Lobster"},
  {username:"Owner", password:"Frog"}];
 function checkpassword(username,password,auth){
 return auth.username==username && auth.password==password;
@@ -22,7 +22,6 @@ return auth.username==username && auth.password==password;
 document.onkeypress = function (e) { if(e.key == "Enter" && login.focus){
   unlockpage();
 }};
-
 function unlockpage(){
       page_cookie.login=true; 
       document.cookie=JSON.stringify(page_cookie);
@@ -37,4 +36,3 @@ if (checkpassword(user.value,password.value,auth[i])){
       }
     }
 }
-    
