e04fae04bad47116a61dddd15024379078afbbc1
Nintendoboi22 <131544102+Nintendoboi22@users.noreply.github.com>
2024-01-30 09:40:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.onkeypress = function (e) { if(e.key == "~") window.location = "https://www.classroom.google.com/h" };
+//document.onkeypress = function (e) { if(e.key == "~") window.location = "https://www.classroom.google.com/h" };
 
 let user=document.getElementById("txt_user")
 let password=document.getElementById("txt_pass")
