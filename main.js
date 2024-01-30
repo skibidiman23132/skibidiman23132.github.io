d8d0ed4e5e8064bd3baa0ee37d908186cb48dd8f
Nintendoboi22 <131544102+Nintendoboi22@users.noreply.github.com>
2024-01-30 10:26:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.onkeypress = function (e) { if(e.key == "~") window.location = "https://www.classroom.google.com/h" };
-
+/*
 let user=document.getElementById("txt_user")
 let password=document.getElementById("txt_pass")
 let button=document.getElementById("imdone")
