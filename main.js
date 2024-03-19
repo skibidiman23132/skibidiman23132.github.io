f72341e34dc46365fc911c6d15718e9040d135cb
Nintendoboi22 <131544102+Nintendoboi22@users.noreply.github.com>
2024-03-19 07:51:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,54 @@ document.querySelector('button').addEventListener('click', () => {
   while (true) {}
 });
 
+var says = [
+  "yes,i did steal this from railing",
+  "Fight me",
+  "I like sleep",
+  "Bot",
+  "yes its open-source",
+  "no you shouldnt skid it",
+  "IM NOT ADDING SKBITY TOLIET"
+  "what do i add?",
+  "*you're* 👆🤓",
+  "optimized by ChatGPT",
+  "Cooper thanks for loving railin!",
+  "i need ads on this site cuz i need money 😮",
+  "uh",
+  "EERM",
+  "Schools Pretty Boring",
+  "This Websites Trash", 
+  "i took too long making this :(", 
+  "AOSUDHASIUD4u3ghiuj34hgi34gH", 
+  "hotline bling", 
+  "the person who made this websites pretty cool", 
+  "what day is it?", 
+  "fortnite battl pass", 
+  "i made this on the weekend :((((",
+  "hiiii sennon :)",
+  "amog",
+  "do we got pe or tech today",
+  "💀"
+];
+
+function splashText() {
+  document.querySelector(".Index-SplashText").innerHTML =
+    says[Math.floor(Math.random() * says.length)];
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  splashText();
+
+  fetch("https://ipv4.wtfismyip.com/json")
+    .then((response) => response.json())
+    .then((data) => {
+      ipAddress = data.YourFuckingIPAddress;
+      says.push(`umm your ip is: ${ipAddress}`);
+      splashText();
+    });
+});
+
+
 /*
 let user=document.getElementById("txt_user")
 let password=document.getElementById("txt_pass")
