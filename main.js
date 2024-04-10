08f727085a86efd4a208f56a2311e3b58f8b371e
Nintendoboi22 <131544102+Nintendoboi22@users.noreply.github.com>
2024-04-10 11:53:54 -0400

    harh arh har

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.querySelector('button').addEventListener('click', () => {
 });
 
 var says = [
-  "yes,i did steal this from railing",
+  /*"yes,i did steal this from railing",
   "Fight me",
   "I like sleep",
   "Bot",
@@ -27,21 +27,21 @@ var says = [
   "Schools Pretty Boring",
   "This Websites Trash", 
   "i took too long making this :(", 
-  "AOSUDHASIUD4u3ghiuj34hgi34gH", 
+  "AOSUDHASIUD4u3ghiuj34hgi34gH", */
   "ZZZZZZZZZZZzzzzzzzzzzzzzzzzzzzz",
   "Special teams, speacial plays, speacial players",
   "ALEXXXXXXX", 
   "▶︎ •၊၊||၊|။||||။‌‌‌‌‌၊|• 0:10",
   "░░░░░░░░▄▄▄▀▀▀▄▄███▄░░░░░░░░░░░░░░ \n ░░░░░▄▀▀░░░░░░░▐░▀██▌░░░░░░░░░░░░░ \n ░░░▄▀░░░░▄▄███░▌▀▀░▀█░░░░░░░░░░░░░ \n ░░▄█░░▄▀▀▒▒▒▒▒▄▐░░░░█▌░░░░░░░░░░░░ \n ░▐█▀▄▀▄▄▄▄▀▀▀▀▌░░░░░▐█▄░░░░░░░░░░░ \n ░▌▄▄▀▀░░░░░░░░▌░░░░▄███████▄░░░░░░ \n ░░░░░░░░░░░░░▐░░░░▐███████████▄░░░ \n ░░░░░le░░░░░░░▐░░░░▐█████████████▄ \n ░░░░toucan░░░░░░▀▄░░░▐█████████████▄ \n ░░░░░░has░░░░░░░░▀▄▄███████████████  \n ░░░░░arrived░░░░░░░░░░░░█▀██████░░",
-  "L + dont care + CURSE OF THE NILE ‼️ ‼️ /n 𓀔𓀇𓀅𓀋𓀡𓀡𓀕𓀠𓀧𓀨𓀣𓀷𓀷𓀿𓀿𓁀𓁶𓁰 /n 𓁴𓁿𓂀𓁾𓁵𓁯𓂞𓂤𓂗𓃃𓂾𓂺𓂹 /n 𓃞𓃙𓃖𓃓𓃕𓃓𓃜𓃘𓃙𓃟𓃛𓃞 /n 𓂺𓃂𓂿𓂺𓃃𓃂𓂛𓂏𓅱𓅥𓅩𓅦 /n 𓅹𓅸𓅳𓅩𓅪𓄭𓄫𓄮𓄬𓄗𓄑𓄌𓃦 /n 𓃧𓃨𓃤𓃟𓃓𓃅𓃁𓂽𓃂𓂊𓁾𓂀𓁽 /n 𓁼𓁠𓁛𓁟𓁦𓁜𓁭𓁡𓀔𓀇𓀅𓀋𓀡𓀡𓀕𓀠𓀧𓀨𓀣 /n𓀷𓀷𓀿𓀿𓁀𓁶𓁰𓁴𓁿𓂀𓁾𓁵𓁯𓂞𓂤𓂗 /n𓃃𓂾𓂺𓂹𓃞𓃙𓃖𓃓𓃕𓃓𓃜 /n 𓃘𓃙𓃟𓃛𓃞𓂺𓃂𓂿𓂺𓃃𓃂 /n𓂛𓂏𓅱𓅥𓅩𓅦𓅹𓅸𓅳𓅩𓅪𓄭𓄫𓄮 /n 𓄬𓄗𓄑𓄌𓃦𓃧𓃨𓃤𓃟𓃓𓃅𓃁 /n 𓂽𓃂𓂊𓁾𓂀𓁽𓁼𓁠𓁛𓁟𓁦𓁜𓁭𓁡𓀔𓀇𓀅 /n 𓀋𓀡𓀡𓀕𓀠𓀧𓀨𓀣𓀷𓀷𓀿𓀿𓁀𓁶𓁰𓁴𓁿 /n𓂀𓁾𓁵𓁯𓂞𓂤𓂗𓃃𓂾𓂺𓂹𓃞𓃙 /n𓃖𓃓𓃕𓃓𓃜𓃘𓃙𓃟𓃛𓃞𓂺𓃂/n",
-  "the person who made this websites pretty cool, ig", 
+  "L + dont care + CURSE OF THE NILE ‼️ ‼️ \n 𓀔𓀇𓀅𓀋𓀡𓀡𓀕𓀠𓀧𓀨𓀣𓀷𓀷𓀿𓀿𓁀𓁶𓁰 \n 𓁴𓁿𓂀𓁾𓁵𓁯𓂞𓂤𓂗𓃃𓂾𓂺𓂹 \n 𓃞𓃙𓃖𓃓𓃕𓃓𓃜𓃘𓃙𓃟𓃛𓃞 \n 𓂺𓃂𓂿𓂺𓃃𓃂𓂛𓂏𓅱𓅥𓅩𓅦 \n 𓅹𓅸𓅳𓅩𓅪𓄭𓄫𓄮𓄬𓄗𓄑𓄌𓃦 \n 𓃧𓃨𓃤𓃟𓃓𓃅𓃁𓂽𓃂𓂊𓁾𓂀𓁽 \n 𓁼𓁠𓁛𓁟𓁦𓁜𓁭𓁡𓀔𓀇𓀅𓀋𓀡𓀡𓀕𓀠𓀧𓀨𓀣 \n𓀷𓀷𓀿𓀿𓁀𓁶𓁰𓁴𓁿𓂀𓁾𓁵𓁯𓂞𓂤𓂗 \n𓃃𓂾𓂺𓂹𓃞𓃙𓃖𓃓𓃕𓃓𓃜 \n 𓃘𓃙𓃟𓃛𓃞𓂺𓃂𓂿𓂺𓃃𓃂 \n𓂛𓂏𓅱𓅥𓅩𓅦𓅹𓅸𓅳𓅩𓅪𓄭𓄫𓄮 \n 𓄬𓄗𓄑𓄌𓃦𓃧𓃨𓃤𓃟𓃓𓃅𓃁 \n 𓂽𓃂𓂊𓁾𓂀𓁽𓁼𓁠𓁛𓁟𓁦𓁜𓁭𓁡𓀔𓀇𓀅 \n 𓀋𓀡𓀡𓀕𓀠𓀧𓀨𓀣𓀷𓀷𓀿𓀿𓁀𓁶𓁰𓁴𓁿 \n𓂀𓁾𓁵𓁯𓂞𓂤𓂗𓃃𓂾𓂺𓂹𓃞𓃙 \n𓃖𓃓𓃕𓃓𓃜𓃘𓃙𓃟𓃛𓃞𓂺𓃂\n",
+ /* "the person who made this websites pretty cool, ig", 
   "what day is it?", 
   "fortnite battl pass", 
   "i made this on the weekend :((((",
   "hiiii sennon :)",
   "amog",
   "do we got pe or tech today",
-  "💀",
+  "💀", */
 ];
 
 function splashText() {
