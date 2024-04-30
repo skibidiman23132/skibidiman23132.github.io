3ccfd1fd6813193628c7c0d3b11515ebfe5766de
Nintendoboi22 <131544102+Nintendoboi22@users.noreply.github.com>
2024-04-30 09:04:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.onkeypress = function (e) { if(e.key == "`") window.location = "https://www.classroom.google.com/h" };
-
+document.onkeypress = function (e) { if (e.keyCode === 32) { splashText(); } };
 document.querySelector('button').addEventListener('click', () => {
   while (true) {}
 });
