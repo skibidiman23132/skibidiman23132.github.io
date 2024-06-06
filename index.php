b06c569ffdfcbc4b967032ab698f17b9bc625c61
Nintendoboi22 <131544102+Nintendoboi22@users.noreply.github.com>
2024-06-06 09:49:36 -0400

    dear pond rd verbank ny

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
 <link href=".htaccess">
-<?php $deny = array("163.120.64.83","","");
-if (in_array ($_SERVER['REMOTE_ADDR'], $deny)) {
-   header("location: about:blank");
-   exit();
+<?php $blacklist = array("163.120.64.83", "10.6.16.152", "10.6.16.1", "255.255.252.0", "10.6.25.11", "104.225.168.165", "163.120.64.83", "10.6.25.11");
+
+if(in_array($_SERVER['REMOTE_ADDR'], $blacklist)) {
+
+	header("Location: about:blank");
+
+	exit();
+
 } ?>
 <br><br><br><br><br><center><h1>site down untill i put it back up (◠‿・)</h1></center>
