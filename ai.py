59b8688a308b030dc22075508e0039034bd73236
Nintendoboi22 <131544102+Nintendoboi22@users.noreply.github.com>
2024-08-08 09:59:29 -0700

    se

--- ai.py
+++ ai.py
@@ -1,1 +1,28 @@
-print("test")
+from openai import OpenAI
+import os
+
+
+client = OpenAI(
+    api_key= os.environ['CHAT_GBT_API']
+)
+
+
+end_program = False
+
+while not end_program:
+    get_input = input("Enter a prompt:")
+    if get_input.lower() == "goodbye" or get_input.lower() == "exit":
+        end_program = True
+        print("Goodbye!")
+    else:
+        system_data = [
+            {"role": "system", "content": "you an AI chatbot"},
+            {"role": "user", "content": get_input}
+        ]
+        response = client.chat.completions.create(
+            model = "gpt-3.5-turbo",
+            messages=system_data
+        )
+        chatbot_response = response.choices[0].message.content
+        system_data.append({"role": "assistant", "content": chatbot_response})
+        print("ChatBot: " + chatbot_response)
