852bdb3895add96e9f36968fc66ddd189c703273
Do_Not_Dusturb <152636544+Do-Not-Dusturb@users.noreply.github.com>
2024-11-30 09:35:53 -0500

    Update Search.js

--- Search.js
+++ Search.js
@@ -3,32 +3,13 @@ function searchgames() {
     input = document.getElementById('searchBar');
     filter = input.value.toUpperCase();
     games = document.getElementsByClassName('game-link');
-    var gameGrid = document.querySelector('.game-grid');
-
-    var matchingGames = [];
-    var nonMatchingGames = [];
 
     for (i = 0; i < games.length; i++) {
         txtValue = games[i].textContent || games[i].innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            matchingGames.push(games[i]);
+            games[i].style.display = "";
         } else {
-            nonMatchingGames.push(games[i]);
+            games[i].style.display = "none";
         }
     }
-
-    // Clear the game grid
-    gameGrid.innerHTML = '';
-
-    // Append matching games first
-    matchingGames.forEach(function(game) {
-        game.style.display = '';
-        gameGrid.appendChild(game);
-    });
-
-    // Append non-matching games
-    nonMatchingGames.forEach(function(game) {
-        game.style.display = '';
-        gameGrid.appendChild(game);
-    });
 }
