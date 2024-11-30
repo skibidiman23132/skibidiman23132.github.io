8fdc0f4dcb6bacb9bc2269ee7e9c103dc17dfb94
Do_Not_Dusturb <152636544+Do-Not-Dusturb@users.noreply.github.com>
2024-11-30 09:24:47 -0500

    Update Search.js

--- Search.js
+++ Search.js
@@ -1,15 +1,34 @@
 function searchgames() {
-        var input, filter, games, i, txtValue;
-        input = document.getElementById('searchBar');
-        filter = input.value.toUpperCase();
-        games = document.getElementsByClassName('game-link');
+    var input, filter, games, i, txtValue;
+    input = document.getElementById('searchBar');
+    filter = input.value.toUpperCase();
+    games = document.getElementsByClassName('game-link');
+    var gameGrid = document.querySelector('.game-grid');
 
-        for (i = 0; i < games.length; i++) {
-            txtValue = games[i].textContent || games[i].innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                games[i].style.display = "";
-            } else {
-                games[i].style.display = "none";
-            }
+    var matchingGames = [];
+    var nonMatchingGames = [];
+
+    for (i = 0; i < games.length; i++) {
+        txtValue = games[i].textContent || games[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            matchingGames.push(games[i]);
+        } else {
+            nonMatchingGames.push(games[i]);
         }
     }
+
+    // Clear the game grid
+    gameGrid.innerHTML = '';
+
+    // Append matching games first
+    matchingGames.forEach(function(game) {
+        game.style.display = '';
+        gameGrid.appendChild(game);
+    });
+
+    // Append non-matching games
+    nonMatchingGames.forEach(function(game) {
+        game.style.display = '';
+        gameGrid.appendChild(game);
+    });
+}
