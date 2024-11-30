a578f37102bd6d8df9e488bdca36e33e3b6dd3a3
Do_Not_Dusturb <152636544+Do-Not-Dusturb@users.noreply.github.com>
2024-11-30 09:45:42 -0500

    Update Search.js

--- Search.js
+++ Search.js
@@ -1,21 +1,15 @@
 function searchgames() {
-    var input, filter, games, i, txtValue, matchingGames = [];
+    var input, filter, games, i, txtValue;
     input = document.getElementById('searchBar');
     filter = input.value.toUpperCase();
-    games = Array.from(document.querySelectorAll('a')); // Select all <a> elements
+    games = document.getElementsByClassName('game-link');
 
-    // Identify and group matching and non-matching games
     for (i = 0; i < games.length; i++) {
-        var gameLink = games[i].querySelector('.game-link'); // Get .game-link child inside <a>
-        if (gameLink) { // Check if .game-link exists
-            txtValue = gameLink.textContent || gameLink.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                matchingGames.push(games[i]);
-            }
+        txtValue = games[i].textContent || games[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            games[i].style.display = "";
+        } else {
+            games[i].style.display = "none";
         }
     }
-
-    // Reorder the list
-    matchingGames.forEach(game => game.parentNode.appendChild(game)); // Move matching <a> to the top
-    games.filter(game => !matchingGames.includes(game)).forEach(game => game.parentNode.appendChild(game)); // Move non-matching <a> below
 }
